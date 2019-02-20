3e83dbaa81acf4f97fbde0202da649b041cf0c11
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-20 02:29:14 -0500

    se agrego el middleware, a la url /pruebas.

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,11 +2,13 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var md_auth= require('../middlewares/authenticated');
 
 var api = express.Router();
 
 api.get('/home', UserController.home);
-api.get('/register', UserController.saveUser);
+api.get('/pruebas',md_auth.ensureAuth, UserController.prueba);
+api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
 module.exports = api;
