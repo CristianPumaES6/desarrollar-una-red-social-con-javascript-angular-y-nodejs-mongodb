1233e7353d1d5c05c5a019bb0007c294487a52f6
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-20 02:42:29 -0500

    test middleware.

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,13 +2,14 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
-var md_auth= require('../middlewares/authenticated');
+var md_auth = require('../middlewares/authenticated');
 
 var api = express.Router();
 
 api.get('/home', UserController.home);
-api.get('/pruebas',md_auth.ensureAuth, UserController.prueba);
+api.get('/pruebas', md_auth.ensureAuth, UserController.prueba);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.get('/user/:id', md_auth.ensureAuth, UserController.loginUser);
 
 module.exports = api;
