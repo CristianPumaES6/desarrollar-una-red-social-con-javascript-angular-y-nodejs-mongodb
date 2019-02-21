f9b6f09ba39249087109a50ea7b6f7ea1426cecb
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-21 00:05:18 -0500

    SaveFollow, DeleteFollow.

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -5,8 +5,42 @@ var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user');
 var Follow = require('../models/follow');
 
-function prueba(req, res) {
-    res.status(200).send({ message: 'TEST' });
+function saveFollow(req, res) {
+    var params = req.body;
+
+    var follow = new Follow();
+    follow.user = req.user.sub;
+    follow.followed = params.followed;
+
+    follow.save((err, followStored) => {
+        if (err) return res.status(200).send({
+            message: 'Error al guardar saveFollow()'
+        })
+
+        if (!followStored) return res.status(404).send({ message: 'El seguimiento no se a guardado retorno basio.' });
+
+
+        return res.status(200).send({ follow: followStored });
+    });
+
 }
 
-module.exports = { prueba };
+function deleteFollow(req, res) {
+
+    var userId = req.user.sub;
+    var followId = req.params.id;
+
+    Follow.find({ 'user': userId, 'followed': followId }).remove(err => {
+
+        if (err) return res.status(500).send({ message: 'Error al dejar de siguir deleteFollow()' });
+        return res.status(200).send({ message: 'El follow se ha eliminado.' });
+
+    })
+
+
+}
+
+module.exports = {
+    saveFollow,
+    deleteFollow
+};
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -6,6 +6,7 @@ var api = express.Router();
 
 var md_auth = require('../middlewares/authenticated');
 
-api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 
 module.exports = api;
