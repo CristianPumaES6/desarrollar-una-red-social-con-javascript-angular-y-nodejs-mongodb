e4b353eb7b74955eb9a9db7376196473bfad08ff
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-20 22:44:07 -0500

    Controlador de rutas y seguimiento

---
+++ api/controllers/follow.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var mongoosePaginate = require('mongoose-pagination');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+
+function prueba(req, res) {
+    res.status(200).send({ message: 'TEST' });
+}
+
+module.exports = { prueba };
