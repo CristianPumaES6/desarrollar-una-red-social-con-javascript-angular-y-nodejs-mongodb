e30fd8695da5b61560ef772147cb416366957790
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-27 18:51:54 -0500

    Enviar mensaje.

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -11,6 +11,26 @@ function probando(req, res) {
     res.status(200).send({ message: 'HOLA QUE TAL ?' });
 }
 
+function saveMessage(req, res) {
+    var params = req.body;
+
+    if (!params.text || !params.receiver) return res.status(200).send({ message: 'Enviar los datos necesarios.' });
+
+    var message = new Message();
+    message.emitter = req.user.sub;
+    message.receiver = params.receiver;
+    message.text = params.text;
+    message.create_at = moment().unix();
+
+    message.save((err, messageStored) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición.' });
+        if (!messageStored) return res.status(500).send({ message: 'Error en la petición.' });
+
+        return res.status(200).send({ message: messageStored });
+    });
+}
+
 module.exports = {
-    probando
+    probando,
+    saveMessage
 }
