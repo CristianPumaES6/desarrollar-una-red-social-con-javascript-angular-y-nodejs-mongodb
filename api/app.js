5bcb09d4bc434a7d281a55600b20468bed761958
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-26 20:03:09 -0500

    Controlador y rutas de publicaciones.

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ var app = express();
 //cargar rutas
 var user_routes = require('./routes/user')
 var follow_routes = require('./routes/follow')
+var publication_routes = require('./routes/publication')
 
 //--------- Middlewares -------------
 // parse application/x-www-form-urlencoded
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 //rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
+app.use('/api', publication_routes);
 
 
 //exportar
