4001693bcd7b73faefdeb9739e77036cd7538815
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-07 17:17:02 -0500

    Se agrego el Usuario model al componente Register.

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public title: string;
+  public user: User;
 
   constructor() {
-    this.title = 'Register';
+    this.title = 'Registrate';
+    this.user = new User(
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "ROLE_USER",
+      "");
   }
 
   ngOnInit() {
