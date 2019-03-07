368eb82f6595a00d55a3683b9f59baf46c3dc19c
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-07 13:57:47 -0500

    Componente register solucionado.

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public title: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Register';
+  }
 
   ngOnInit() {
+    console.log('Componente de Register cargado..');
   }
 
 }
