0c936877cb09deab72862631709a2712dc0bb341
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-08 15:25:12 -0500

    Identificar un usuario.

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -10,20 +10,74 @@ import { UserService } from '../../services/user.service'
 
 export class LoginComponent implements OnInit {
     public title: string;
-    public user:User;
+    public user: User;
+    public status: string;
+    public identity;
+    public token;
 
     constructor(
-        private _userService:UserService
+        private _userService: UserService
     ) {
         this.title = 'Identificate';
-        this.user = new User("","","","","","","ROLE_USER","");
+        this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
     }
 
     ngOnInit() {
         console.log('Componente de login cargado..')
     }
 
-    onSubmit(){
-        console.log("Enviar datos")
+    onSubmit() {
+        //logear al usuario y consegir sus datos.
+        this._userService.singup(this.user).subscribe(
+            response => {
+                this.identity = response.user;
+
+                console.log(this.identity);
+
+                if (!this.identity || !this.identity._id) {
+                    this.status = 'error';
+                } else {
+                    this.status = 'success';
+                    // PERSISTIR DATOS DEL USUARIO
+
+                    //Conseguir el token
+                    this.getToken();
+                }
+            }, error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if (errorMessage != null) {
+                    this.status = 'error';
+                }
+            }
+        )
+    }
+
+    getToken() {
+        //logear al usuario y consegir sus datos.
+        this._userService.singup(this.user, 'true').subscribe(
+            response => {
+                this.token = response.token;
+                console.log(this.token);
+
+                if (this.token.length <= 0) {
+                    this.status = 'error';
+                } else {
+                    this.status = 'success';
+                    // PERSISTIR TOKEN DEL USUARIO
+
+                    //Conseguir el contadores o estadisticas de usuario.
+
+                }
+            }, error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if (errorMessage != null) {
+                    this.status = 'error';
+                }
+            }
+        )
     }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -18,4 +18,15 @@ export class UserService {
 
         return this._http.post(this.url + 'register', params, { headers })
     }
+
+    singup(user: User, gettoken = null): Observable<any> {
+        if (gettoken != null) {
+            user = Object.assign(user, {gettoken});
+        }
+
+        let params = JSON.stringify(user);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.post(this.url + 'login', params, { headers });
+    }
 }
