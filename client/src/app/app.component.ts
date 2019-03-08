69a1a6f160c70e152ae6a5eb61e5c54325f284b2
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-08 16:31:32 -0500

    Persistir la sesion del Usuario.  ngDoCheck() ,  ngOnInit()

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { UserService } from './services/user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'CPSocial';
+
+export class AppComponent implements OnInit, DoCheck {
+  public title: string;
+  public identity;
+
+  constructor(
+    private _userService: UserService
+  ) {
+    this.title = 'CPSocial';
+  }
+
+  ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    console.log(this.identity);
+  }
+  //cada vez que haiga un cambio se ejecuta el DoCheck
+  ngDoCheck() {
+    this.identity = this._userService.getIdentity();
+  }
 }
