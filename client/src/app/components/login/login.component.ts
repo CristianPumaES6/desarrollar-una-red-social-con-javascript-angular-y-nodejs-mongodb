1af058b1b9d2035072b6bf203e832255b696910e
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-07 23:18:46 -0500

    Creacion del formulario login mas onSubmit().

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service'
 
 @Component({
     selector: 'login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    providers: [UserService]
 })
 
-export class LoginComponent implements OnInit{
-    public title:string;
+export class LoginComponent implements OnInit {
+    public title: string;
+    public user:User;
 
-    constructor(){
+    constructor(
+        private _userService:UserService
+    ) {
         this.title = 'Identificate';
+        this.user = new User("","","","","","","ROLE_USER","");
     }
 
-    ngOnInit(){
+    ngOnInit() {
         console.log('Componente de login cargado..')
+    }
+
+    onSubmit(){
+        console.log("Enviar datos")
     }
 }
