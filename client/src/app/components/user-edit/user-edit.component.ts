721aeb9c953ed37e20cb7f200925c0c229ca7b5d
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-09 00:22:53 -0500

    Componente y ruta a mis-datos.

--- client/src/app/components/user-edit/user-edit.component.ts
+++ client/src/app/components/user-edit/user-edit.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+    private title: string;
+    public user: User;
+    public identity;
+    public token;
+    public status: string;
+    
+  constructor(
+    private _route: ActivatedRoute,
+    private _routers: Router,
+    private _userService: UserService
+  ) {
+    this.title = 'Actualizar mis datos';
+    this.user = this._userService.getIdentity();
+    this.identity = this.user;
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit() {
+    console.log(this.user);
+    console.log("user.edith component se ha cargado.");
   }
 
 }
