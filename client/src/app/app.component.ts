1d4198d4f646c9672409d44353420e384751b5e7
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-10 01:10:44 -0500

    Mostrar avatar en la barra de menu.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService } from './services/user.service';
+import { GLOBAL } from './services/global';
 
 
 @Component({
@@ -13,13 +14,15 @@ import { UserService } from './services/user.service';
 export class AppComponent implements OnInit, DoCheck {
   public title: string;
   public identity;
+  public url: string;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router:Router,
+    private _router: Router,
     private _userService: UserService
   ) {
     this.title = 'CPSocial';
+    this.url = GLOBAL.url;
   }
 
   ngOnInit() {
