1cee6cc83fe435060c7453d18a2161a1dee41e4e
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-11 15:37:39 -0500

    Crear formulario de publicaciones.

--- client/src/app/componets/sidebar/sidebar.component.ts
+++ client/src/app/componets/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { GLOBAL } from '../../services//global';
+import { GLOBAL } from '../../services/global';
+
+import { Publication } from '../../models/publication';
 
 @Component({
   selector: 'app-sidebar',
@@ -14,19 +16,25 @@ export class SidebarComponent implements OnInit {
   public stats;
   public url;
   public status;
+  public publication: Publication;
 
   constructor(
     private _userService: UserService
-  ) { 
+  ) {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.stats = this._userService.getStats();
     this.url = GLOBAL.url;
+    this.publication = new Publication("", "", "", "", this.identity._id);
   }
 
   ngOnInit() {
     console.log("El componente SideBar a sido cargado.")
-    console.log(this.stats)
+    console.log(this.publication)
+  }
+  onSubmit(){
+    
+    console.log(this.publication)
   }
 
 }
